0a4fd61416e459bd9091552d11e865f35e64a9f0
Kartik Kenche <119101233+Kartikkenche@users.noreply.github.com>
2025-01-24 14:01:17 +0530

    Add files via upload

---
+++ Wheather app/Script.js
@@ -0,0 +1,109 @@
+const searchbar = document.querySelector(".search");
+const submitBtn = document.querySelector(".search-button");
+const parentContainer = document.querySelector(".container");
+
+// UX for Mouse Users
+submitBtn.addEventListener("click", (e) => {
+  getCondition(searchbar.value);
+  searchbar.value = "";
+});
+
+// UX for Keyboard Users
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    getCondition(searchbar.value);
+    searchbar.value = "";
+  }
+});
+
+async function getCondition(location) {
+  const key = "03a312df56c6ea172c35ee97622df898";
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${key}`;
+
+  fetch(api)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayCondition)
+    .catch((err) => {
+      console.log(err);
+      alert("Please enter a correct city or country!");
+    });
+}
+
+function displayCondition(weather) {
+  let presentDay = new Date();
+  const iconLink = "https://openweathermap.org/img/w/";
+
+  let weatherResult = `
+  <main class="weather-result">
+    <section class="location-date">
+      <div class="city">${weather.name}, ${weather.sys.country}</div>
+      <div class="date">${getDate(presentDay)}</div>
+    </section>
+    <div class="temp-details">
+      <div class="temp-icon">
+      <img src="${iconLink}${weather.weather[0].icon}.png" />
+      </div>
+      <div class="temperature">
+      ${Math.round(weather.main.temp)}<span class="temp-symbol">&deg;c</span>
+      </div>
+      <div class="condition">${weather.weather[0].main}</div>
+
+      <div class="temp-degree">
+        <div class="subdegree">
+          <p>Low</p>
+          <p>High</p>
+        </div>
+      
+        ${Math.round(weather.main.temp_min)}&deg;c / ${Math.round(
+    weather.main.temp_max
+  )}&deg;c
+      </div>
+    </div>
+    </main>
+  `;
+
+  const container = document.querySelector(".weather-result");
+
+  if (container) {
+    container.remove();
+    parentContainer.insertAdjacentHTML("beforeend", weatherResult);
+  } else {
+    parentContainer.insertAdjacentHTML("beforeend", weatherResult);
+  }
+}
+
+function getDate(now) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let day = days[now.getDay()];
+  let date = now.getDate();
+  let month = months[now.getMonth()];
+  let year = now.getFullYear();
+
+  return `${day}, ${date} ${month} ${year} `;
+}
